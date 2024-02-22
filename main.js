d403342e6be19028bb14bfe36566863d89c08ae9
Stefanio96 <stepan.mahochii@gmail.com>
2024-02-22 13:57:27 +0200

    add file index.html add file main.js add new info in file main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+let z1 ="'hello' "
+
+console.log(z1)
+let z2= "'owu'"
+
+console.log(z2)
+
+let z3="'com' "
+console.log(z3)
+
+let z4= "'ua'"
+console.log(z4)
+
+let z5 =1
+console.log(z5)
+
+let z6=10
+console.log(z6)
+
+let z7=-999
+console.log(z7)
+
+let z8=123
+console.log(z8)
+
+let z9=3.14
+console.log(z9)
+
+let z10=2.7
+console.log(z10)
+
+let z11=16
+console.log(z11)
+
+let z12=true
+console.log(z12)
+
+let z13=false
+console.log(z13)
+
+/**************************************************************************************************************/
+
+let firstName='Магочій'
+
+let middleName='Степан'
+
+let lastName='Степанович'
+
+let person=`${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+let a=100
+console.log(typeof a,a)
+
+let b="100"
+console.log(typeof b,b)
+
+let c= true
+console.log(typeof c,c)
