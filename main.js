60e648ccd0d208efaeaf3d8d24de99e3af71da87
Stefanio96 <stepan.mahochii@gmail.com>
2024-03-25 12:55:00 +0200

    add file index.html add file main.js add new info in file main.js add new file users.html add new file eachUser.html

--- main.js
+++ main.js
@@ -1,149 +1,84 @@
-// Стоврити форму з трьома полями для name,surname,age та кнопкою.
-// При натисканні на кнопку зчитати данні з полів, та вивести об'єкт в документ. Іншими словами : заповниои форму, натиснули кнопку, під формою з'явився блок з вашим об'єктом
-
-// function submitForm() {
-//     let formData = {
-//         name: document.getElementById("name").value,
-//         surname: document.getElementById("surname").value,
+// - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
 //
-//     }
+// fetch('https://dummyjson.com/carts')
+//     .then(res => res.json())
+//     .then(value => {
+//         let { carts } = value
+//         console.log(value)
+//         for (const cart of carts) {
+//             let block = document.createElement('div')
 //
-//     // Отриманий об'єкт даних форми виводимо під формою
-//     let resultDiv = document.getElementById("result")
-//     resultDiv.innerHTML = "<pre>" + JSON.stringify(formData, null, 2) + "</pre>"
-// }
-
-// ==========================
-// є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
+//             let h2 = document.createElement("h2")
+//             h2.innerText = `Box Id: ${cart.id}, Total: ${cart.total}, Total Discount: ${cart.discountedTotal}, UserId: ${cart.userId}`
+//             block.appendChild(h2)
 //
-// let numberElement = document.getElementById("number")
+//             let ul = document.createElement("ul")
 //
-// let currentNumber = parseInt(numberElement.textContent)
+//             for (const product of cart.products) {
+//                 let li = document.createElement('li')
+//                 li.innerText = `Id: ${product.id}, Title: ${product.title}, Price: ${product.price}, Quantity: ${product.quantity}, Total; ${product.total}`
+//                 ul.appendChild(li)
+//             }
 //
-// let newNumber = currentNumber + 1
+//             block.appendChild(ul)
+//             document.body.appendChild(block)
+//         }
+//     })
 //
-// numberElement.textContent = newNumber
-//
-//
-//  ==========================
-// Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація про дату та час відвідування сторінки.
-// Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html.
-// Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
-
-
-// Зробив!
-
-// =========================
-//     зробити масив на 100 об'єктів та дві кнопки prev next
-// при завантажені сторінки з'являються перші 10 об'єктів.
-//     При натисканні next виводяться настпні 10 об'єктів
-// При натисканні prev виводяться попередні 10 об'єктів
-
-
-// const objJson = [
-//     { adName: "AdName 1"},
-//     { adName: "AdName 2"},
-//     { adName: "AdName 3"},
-//     { adName: "AdName 4"},
-//     { adName: "AdName 5"},
-//     { adName: "AdName 6"},
-//     { adName: "AdName 7"},
-//     { adName: "AdName 8"},
-//     { adName: "AdName 9"},
-//     { adName: "AdName 10"},
-//     { adName: "AdName 2"},
-//     { adName: "AdName 3"},
-//     { adName: "AdName 4"},
-//     { adName: "AdName 5"},
-//     { adName: "AdName 32"},
-//     { adName: "AdName 73"},
-//     { adName: "AdName 84"},
-//     { adName: "AdName 95"},
-//     { adName: "AdName 10"},
-//     { adName: "AdName 32"},
-//     { adName: "AdName 36"},
-//     { adName: "AdName 48"},
-//     { adName: "AdName 5"},
-//     { adName: "AdName 60"},
-//     { adName: "AdName 76"},
-//     { adName: "AdName 88"},
-//     { adName: "AdName 94"},
-//     { adName: "AdName 10"},
-// ]
-//
-//
-// const parentButton = document.getElementById("parentButton")
-// const prevButton = document.getElementById("prev")
-// const nextButton = document.getElementById("next")
-//
-// let startIndex=0
-//
-//
-// function displayObjects(startIndex) {
-//     parentButton.innerHTML = ""
-//     for (let i = startIndex; i < startIndex + 10; i++) {
-//         if (i >= objJson.length) break
-//         const div = document.createElement("div")
-//         div.textContent = objJson[i].adName
-//         parentButton.appendChild(div)
-//     }
-// }
-//
-// displayObjects(startIndex)
-//
-//
-// nextButton.addEventListener("click", function() {
-//     startIndex = Math.min(startIndex + 10, objJson.length - 10)
-//     displayObjects(startIndex)
-// })
-//
-//
-// prevButton.addEventListener("click", function() {
-//     startIndex = Math.max(startIndex - 10, 0)
-//     displayObjects(startIndex)
-// })
-
-
-// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
-
-//
-// document.getElementById('button').addEventListener('click',function (){
-//     let el=document.getElementById('text')
-//     if (el.style.display !=='none'){
-//         el.style.display ='none'
-//     }else {
-//         el.style.display = 'block'
-//     }
-// })
-
-
-
-//     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18,
-//     та повідомити про це користувача
-//
-// document.getElementById('inspectAgeBtn').addEventListener('click',function (){
-//     let age=document.getElementById('age').value
-//     if (age >=18){
-//         alert('access is allowed')
-//     }else {
-//         alert('Access is denied')
-//     }
-// })
-//
-// *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
-//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
-// (Додаткова частина для завдання)
-
 
 
 //
-// *** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком, в середині якого є значення "100грн"
-// при перезавантаженні сторінки до значаення додається по 10грн, але !!!
-//     зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
-//     При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
+
+    // - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
 
 
+//
+// fetch('https://dummyjson.com/recipes')
+//     .then(res => res.json())
+//     .then(value => {
+//         let {recipes}=value
+//
+//
+//         for (const recipe of recipes) {
+//             let block=document.createElement('div')
+//
+//             let h2=document.createElement("h2")
+//             h2.innerText=recipe.id
+//             block.appendChild(h2)
+//
+//
+//
+//             let h1=document.createElement('h1')
+//             h1.innerText=recipe.name
+//             block.appendChild(h1)
+//
+//             let img=document.createElement('img')
+//             img.src=recipe.image
+//             block.appendChild(img)
+//
+//
+//             let ul=document.createElement("ul")
+//             let ingredients=recipe.ingredients
+//
+//
+//             for (const ingredient of ingredients) {
+//                 let li=document.createElement('li')
+//                 li.innerText=ingredient
+//                 ul.appendChild(li)
+//
+//
+//             }
+//             block.appendChild(ul)
+//             document.body.appendChild(block)
+//
+//         }
+//     });
 
+//
+//     - зробити файл users.html
+// з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
+//     вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
+// при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
+// отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   де ХХХ - айді користувача)
 
-
-
+// ready
